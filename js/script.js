6da0065a07bcd35cd808c33f24b3dd0cb4a96194
burillu <burillu90@hotmail.it>
2023-11-03 16:39:35 +0100

    add function newMsg, and auto answer ok

--- js/script.js
+++ js/script.js
@@ -1,37 +1,55 @@
-import {contactList} from './data.js';
+import { contactList } from './data.js';
 import { findElement } from './utility.js';
 
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!',
-        contacts: contactList,
-        user:{
-          id: 'user',
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!',
+      contacts: contactList,
+      user: {
+        id: 'user',
         name: 'Michele',
         avatar: 'img/avatar_1.jpg',
-        
-        },
-        activeContactIndex:0
-      }
-    },
-    methods:{
-      setContactIndex(ident){
-        const index= findElement(ident,this.contacts);
-        this.activeContactIndex= index;
-      }
-    },
-    computed:{
-      getActiveContact(){
-        return this.contacts[this.activeContactIndex]
+
       },
-      getActiveMsg(){
-        return this.getActiveContact.messages;
+      activeContactIndex: 0,
+      newMsgInput: ''
+    }
+  },
+  methods: {
+    setContactIndex(ident) {
+      const index = findElement(ident, this.contacts);
+      this.activeContactIndex = index;
+    },
+    newMessage() {
+
+      this.getActiveMsg.push(this.msgBuilder(this.newMsgInput, 'sent'));
+      this.newMsgInput = '';
+      setTimeout(this.autoAnswer, 1000)
+
+    },
+    autoAnswer() {
+      this.getActiveMsg.push(this.msgBuilder('ok', 'received'))
+    },
+    msgBuilder(str, status) {
+      return {
+        date: '02/11/2023 15:30:55',
+        message: str,
+        status: status
       }
     }
-  }).mount('#app');
-  
-  
-  //console.log(contactList)
+  },
+  computed: {
+    getActiveContact() {
+      return this.contacts[this.activeContactIndex]
+    },
+    getActiveMsg() {
+      return this.getActiveContact.messages;
+    }
+  }
+}).mount('#app');
+
+
+//console.log(contactList)
